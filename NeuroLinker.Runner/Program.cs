088d35c158b4fef4f4051804fe093440f6abd8a1
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2018-10-31 18:54:04 +0200

    fix: Nasty hack. Fixes #28
    - Adjust code to cater for both the test and actual HTML (desperate here, so screw you)
    - Added a console project that can be used to run actual NeuroLinker retrievals

---
+++ NeuroLinker.Runner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuroLinker.Factories;
+using NeuroLinker.Helpers;
+
+namespace NeuroLinker.Runner
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static async Task Main(string[] args)
+        {
+            var httpClientFactory = new HttpClientFactory();
+            var pageRetriever = new PageRetriever(httpClientFactory);
+            var requestProcessor = new NeuroLinker.Workers.RequestProcessor(pageRetriever);
+
+            var character = await requestProcessor.DoCharacterRetrieval(36828);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        #endregion
+    }
+}
